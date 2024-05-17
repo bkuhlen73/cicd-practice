9f20b4849aeb456c45247397bc6f917b29618982
Bernhard Kuhlen <bernhard.kuhlen@covestro.com>
2024-05-17 14:40:52 +0200

    BK

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,11 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('bkuhlen73/cicd-practice', 'main'),
@@ -17,6 +18,11 @@ export class CdkCicdStack extends cdk.Stack {
         ],
         primaryOutputDirectory: 'cdk.out'
       })
-    })
+    });
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage', {
+      stageName: 'test'
+    }))
+
   }
 }
